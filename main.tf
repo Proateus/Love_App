297439bddaca034640da0f8215839b7214e44b9e
unknown <Tanishka@maneeshku2022gmail.onmicrosoft.com>
2025-07-07 10:05:34 +0530

    add a subnet

--- main.tf
+++ main.tf
@@ -8,3 +8,9 @@ resource "azurerm_virtual_network" "vnet_block" {
   resource_group_name = azurerm_resource_group.rg_block.name
   location = azurerm_resource_group.rg_block.location
 }
+resource "azurerm_subnet" "subnet_block" {
+  name                 = "mera_subnet"
+  resource_group_name  = azurerm_resource_group.rg_block.name
+  virtual_network_name = azurerm_virtual_network.vnet_block.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
