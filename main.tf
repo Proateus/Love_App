b6e3d4e7f4f0e4d7ffc24ab16a4d34d051d4903e
unknown <Tanishka@maneeshku2022gmail.onmicrosoft.com>
2025-07-11 08:29:12 +0530

    add nic

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ resource "azurerm_public_ip" "pip_block" {
   resource_group_name = azurerm_resource_group.rg_block.name
   allocation_method   = "Static"
 }
+resource "azurerm_network_interface" "nic_block" {
+  name                = "mera_nic"
+  location            = azurerm_resource_group.rg_block.location
+  resource_group_name = azurerm_resource_group.rg_block.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_block.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_block.id
+  }
+  
+}
